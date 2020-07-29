1cf91073b49d42907f15c7173ab4f192c46cb736
Wang Zizhou <wzz_714105382@icloud.com>
2020-07-29 18:13:22 +0800

    new examples to check old bug.

--- feature/iteratelearn.go
+++ feature/iteratelearn.go
@@ -1,17 +1,22 @@
 package feature
 
+import (
+	"fmt"
+	"os"
+)
+
 // @author  wzz_714105382@icloud.com
 // @date  2020/7/24 21:58
 // @description
 // @version
 func LearnIterate() {
 	problemSt()
-	rs := []int{1, 2, 3}
-	for ix := range rs {
-		if ix+1 < 1 {
-			panic("Impossible Panic???")
-		}
-	}
+	problemSt2()
+	problemSt3()
+	problemSt4()
+	problemSt5()
+	problemSt6()
+	os.Exit(20)
 }
 
 func problemSt() {
@@ -23,6 +28,70 @@ top:
 			return
 		}
 		println(i)
+		goto top
+	}
+}
+
+func problemSt2() {
+	rates := []int32{1, 2, 3, 4, 5, 6}
+	var sink [6]int
+	j := len(sink)
+	for star := range rates {
+		if star+1 < 1 {
+			panic("")
+		}
+		j--
+		sink[j] = j
+	}
+
+	fmt.Println(sink)
+}
+func problemSt3() {
+	rs := []int{1, 2, 3}
+	for ix := range rs {
+		if ix+1 < 1 {
+			panic("Impossible Panic???")
+		}
+	}
+}
+
+func problemSt4() {
+	i := 5
+top:
+	i++
+	if i < 6 {
+		return
+	}
+	if i > 6 {
+		return
+	}
+	println(i)
+	goto top
+}
+
+func problemSt5() {
+	i := 5
+top:
+	i++
+	println(i)
+	if !(i < 6 || i > 6) {
+		goto top
+	}
+
+}
+
+func problemSt6() {
+	i := 0
+	var sink [10]int
+	j := len(sink)
+top:
+	j--
+	sink[j] = j
+	if i < 2 {
+		i++
+		if i < 1 {
+			return
+		}
 		goto top
 	}
 }
